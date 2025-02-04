65bfd7c6d9e19801363f4cab6819fa9bc866d3b6
ITMATIC-HUSO <h.bozkurt@itmatic.nl>
2025-02-04 09:42:30 +0100

    WIP

--- kiosk_skeleton/var/www/html/index.php
+++ kiosk_skeleton/var/www/html/index.php
@@ -1,5 +1,9 @@
 <h1>Kioskbrowser</h1>
 
+<?php 
+  $wifiSSID = passthru("get-ini /boot/kioskbrowser.ini wifi ssid");
+  $wifiPSK = passthru("get-ini /boot/kioskbrowser.ini wifi psk");
+?>
 CPU temperature: <br>
 <?php passthru("sudo vcgencmd measure_temp"); ?>
 <br>
@@ -12,13 +16,16 @@ Throttling status (everything except 0x0 means throttling, get a better power su
 Last heartbeat:
 <?php echo date("Y-m-d H:i:s", filemtime("/dev/shm/heartbeat")); ?>
 <br>
+WiFi SSID / PSK:
+<?php echo $wifiSSID.' / '.$wifiPSK; ?>
+<br>
 
 <br><br>
 <img src="/screenshot.php?<?php echo microtime(); ?>">
 
 <form method="post" action="">
-  <input type="text" name="wifi_ssid" />
-  <input type="text" name="wifi_password" />
+  <input type="text" name="wifi_ssid" value="<?php echo $wifiSSID; ?>" />
+  <input type="text" name="wifi_password" value="<?php echo $wifiPSK; ?>" />
   <button type="submit">Updaten</button>
 </form>
 
@@ -27,6 +34,14 @@ Last heartbeat:
     exec('update-ini /boot/kioskbrowser.ini wifi ssid "'.$_POST['wifi_ssid'].'"'); 
     exec('update-ini /boot/kioskbrowser.ini wifi psk "'.$_POST['wifi_password'].'"'); 
     exec('update-ini /boot/kioskbrowser.ini browser url "https://narrowcasting.itmatic.nl/"'); 
-    echo 'Geupdate. Reboot het apparaat!';
-  }
+    echo 'Geupdate. Apparaat wordt binnen 2 minuten herstart!';
+  } else if(empty($wifiSSID)) {
 ?>
+<script>
+  const req = new XMLHttpRequest();
+  setInterval(function() {
+      req.open("GET", "http://localhost/heartbeat.php");
+      req.send();
+  }, 10000);
+</script>
+  <?php } ?>
